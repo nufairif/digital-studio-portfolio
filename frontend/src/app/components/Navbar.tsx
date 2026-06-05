4cad2fdf9d34910355c28434e30e8a80adeee10b
Nufairi <farhannufairi7@gmail.com>
2026-06-05 14:37:28 +0700

    update scroll logo

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -56,6 +56,7 @@ export function Navbar() {
       >
         <Link
           to="/"
+          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
           style={{
             fontFamily: "'Bricolage Grotesque', sans-serif",
             fontWeight: 700,
